10ef7449d8f2cd04c33cf94ea536d5dab31b8368
r-pedro17 <ragoczy_pedro@hotmail.com>
2026-09-18 13:44:24 +0200

    build: add packages/hub/src/index.ts

---
+++ packages/hub/src/index.ts
@@ -0,0 +1,12 @@
+export { HarnessRunner } from "./client.js";
+export { HarnessServer } from "./server.js";
+export type {
+  ConnectedApp,
+  HarnessServerOptions,
+  HubAddress,
+  Platform,
+  RegisterMessage,
+  RpcError,
+  RpcRequest,
+  RpcResponse,
+} from "./types.js";
